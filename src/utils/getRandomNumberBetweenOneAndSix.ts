240ba7fbcab2d4b54ad277bb4d39835e521d538d
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2025-08-13 21:33:36 +0530

    Improve random number generator

--- src/utils/getRandomNumberBetweenOneAndSix.ts
+++ src/utils/getRandomNumberBetweenOneAndSix.ts
@@ -2,7 +2,9 @@ export function getRandomNumberBetweenOneAndSix(): number {
   if (window.crypto?.getRandomValues) {
     const array = new Uint32Array(1);
     window.crypto.getRandomValues(array);
-    return (array[0] % 6) + 1;
+    const maxUint32 = 0xffffffff;
+    const scaled = Math.floor((array[0] / (maxUint32 + 1)) * 6) + 1;
+    return scaled;
   } else {
     // Fallback
     return Math.floor(Math.random() * 6) + 1;
