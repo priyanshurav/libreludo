036f38e0592e31c2e60f9b1ae657ba93c3900292
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2025-12-20 17:18:34 +0530

    Enhance setTokenTransitionTime function to set transition timing function based on transition time

--- src/utils/setTokenTransitionTime.ts
+++ src/utils/setTokenTransitionTime.ts
@@ -1,5 +1,9 @@
+import { FORWARD_TOKEN_TRANSITION_TIME } from '../game/tokens/constants';
+
 export function setTokenTransitionTime(transitionTime: number) {
   const game = document.querySelector<HTMLDivElement>('.game');
   if (!game) return;
   game.style.setProperty('--token-transition-time', `${transitionTime}ms`);
+  const timingFn = transitionTime === FORWARD_TOKEN_TRANSITION_TIME ? 'ease-in-out' : 'linear';
+  game.style.setProperty('--token-transition-timing-fn', timingFn);
 }
