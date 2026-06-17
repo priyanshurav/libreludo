ada3ca18a3d832cd961057519654279644441f32
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2026-06-18 01:00:50 +0530

    feat(Game): integrate save/load, extract game logic into hooks
    - Hydrate Redux state from storage on mount when loadSave is true
    - Delete save on game end, save state after initial player registration
    - Extract changeTurn and handlePostDiceRoll logic into dedicated hooks,
      removing direct thunk dispatches and useMoveAndCaptureToken from Game
    - Make handleDiceRoll async; conditionally trigger changeTurn based on result
    - Drop "Any progress made will be lost" from exit prompt since saves persist

--- src/pages/Play/components/Game/Game.tsx
+++ src/pages/Play/components/Game/Game.tsx
@@ -2,13 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import { registerNewPlayer, setPlayerSequence } from '../../../../state/slices/playersSlice';
 import { type TPlayerColour } from '../../../../types';
 import Board from '../Board/Board';
-import { useDispatch, useSelector } from 'react-redux';
-import type { AppDispatch, RootState } from '../../../../state/store';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import { hydrateRootState, type AppDispatch, type RootState } from '../../../../state/store';
 import { registerDice } from '../../../../state/slices/diceSlice';
-import { handlePostDiceRollThunk } from '../../../../state/thunks/handlePostDiceRollThunk';
 import GameFinishedScreen from '../GameFinishedScreen/GameFinishedScreen';
-import { changeTurnThunk } from '../../../../state/thunks/changeTurnThunk';
-import { useMoveAndCaptureToken } from '../../../../hooks/useMoveAndCaptureToken';
 import type { TPlayerInitData } from '../../../../types';
 import { useNavigate } from 'react-router-dom';
 import { playerCountToWord } from '../../../../game/players/logic';
@@ -16,23 +13,45 @@ import bg from '../../../../assets/bg.jpg';
 import { usePageLeaveBlocker } from '../../../../hooks/usePageLeaveBlocker';
 import { addToGameInactiveTime, setGameStartTime } from '../../../../state/slices/sessionSlice';
 import styles from './Game.module.css';
+import { useGameStorage } from '../../../../hooks/useGameStorage';
+import { useChangeTurn } from '../../../../hooks/useChangeTurn';
+import { useHandlePostDiceRoll } from '../../../../hooks/useHandlePostDiceRoll';
 
-export const EXIT_MESSAGE = 'Are you sure you want to exit? Any progress made will be lost.';
+export const EXIT_MESSAGE = 'Are you sure you want to exit?';
 
 type Props = {
   initData: TPlayerInitData[];
+  loadSave: boolean;
 };
 
-function Game({ initData }: Props) {
+function Game({ initData, loadSave }: Props) {
   const dispatch = useDispatch<AppDispatch>();
   const boardTileSize = useSelector((state: RootState) => state.board.boardTileSize);
-  const { playerSequence, isGameEnded, playerFinishOrder, currentPlayerColour, players } =
-    useSelector((state: RootState) => state.players);
+  const { playerSequence, isGameEnded, playerFinishOrder, currentPlayerColour } = useSelector(
+    (state: RootState) => state.players
+  );
+  const { retrieveState, saveState, deleteState } = useGameStorage();
   const playersRegisteredInitiallyRef = useRef(true);
   const gameInactiveStartTime = useRef(0);
   const navigate = useNavigate();
-  const moveAndCapture = useMoveAndCaptureToken();
+  const store = useStore<RootState>();
+  const changeTurnFn = useChangeTurn();
+  const handlePostDiceRoll = useHandlePostDiceRoll();
+
   usePageLeaveBlocker(!isGameEnded && import.meta.env.PROD);
+
+  useEffect(() => {
+    if (initData.length !== 0 && !loadSave) return;
+    const { success, result } = retrieveState();
+    if (success) {
+      store.dispatch(hydrateRootState(result));
+    } else {
+      throw result;
+    }
+  }, [retrieveState, initData.length, loadSave, store]);
+  useEffect(() => {
+    if (isGameEnded) deleteState();
+  }, [isGameEnded, deleteState]);
   useEffect(() => {
     if (initData.length === 0) return;
     dispatch(setPlayerSequence({ playerCount: playerCountToWord(initData.length) }));
@@ -53,7 +72,9 @@ function Game({ initData }: Props) {
       dispatch(registerDice(playerSequence[i]));
     }
     playersRegisteredInitiallyRef.current = false;
-  }, [dispatch, playerSequence, initData]);
+    if (!currentPlayerColour) changeTurnFn();
+    else saveState();
+  }, [dispatch, playerSequence, initData, changeTurnFn, currentPlayerColour, saveState]);
 
   useEffect(() => {
     const handlePageVisibilityChange = () => {
@@ -70,14 +91,10 @@ function Game({ initData }: Props) {
     return () => document.removeEventListener('visibilitychange', handlePageVisibilityChange);
   }, [dispatch, isGameEnded]);
 
-  useEffect(() => {
-    if (currentPlayerColour || players.length === 0 || initData.length === 0) return;
-    dispatch(changeTurnThunk(moveAndCapture));
-  }, [currentPlayerColour, dispatch, initData.length, moveAndCapture, players.length]);
-
-  const handleDiceRoll = (colour: TPlayerColour, diceNumber: number) => {
-    if (initData.length === 0) return;
-    dispatch(handlePostDiceRollThunk(colour, diceNumber, moveAndCapture));
+  const handleDiceRoll = async (colour: TPlayerColour, diceNumber: number) => {
+    if (initData.length === 0 && !loadSave) return;
+    const res = await handlePostDiceRoll(colour, diceNumber);
+    if (res?.shouldChangeTurn) changeTurnFn();
   };
 
   const handleExitBtnClick = () => navigate('/');
