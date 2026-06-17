df9246c126852f75220e0702337c41646fdecfc5
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2026-06-17 23:22:54 +0530

    refactor: migrate dice roll logic from Redux thunk to custom hook
    Converts `rollDiceThunk` to the `useRollDice` hook, decoupling the action from Redux middleware and integrating auto-saving via `useGameStorage` after a successful roll.

---
+++ src/hooks/useRollDice.ts
@@ -0,0 +1,33 @@
+import { type RootState, type AppDispatch } from './../state/store';
+import { useCallback } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import type { TPlayerColour } from '../types';
+import { setIsPlaceholderShowing, renewRollBag, setDiceNumber } from '../state/slices/diceSlice';
+import { useGameStorage } from './useGameStorage';
+
+const DICE_PLACEHOLDER_DELAY = 1000;
+
+export const useRollDice = () => {
+  const store = useStore<RootState>();
+  const dispatch = useDispatch<AppDispatch>();
+  const { saveState } = useGameStorage();
+  return useCallback(
+    (colour: TPlayerColour, onDiceRoll: (diceNumber: number) => void) => {
+      if (store.getState().players.isGameEnded) return;
+      dispatch(setIsPlaceholderShowing({ colour, isPlaceholderShowing: true }));
+      setTimeout(() => {
+        const diceState = store.getState().dice;
+        const dice = diceState.dice.find((d) => d.colour === colour);
+        if (diceState.rollBag[colour].length === 0) dispatch(renewRollBag(colour));
+        const bag = store.getState().dice.rollBag[colour];
+        const index = Math.floor(Math.random() * bag.length);
+        const diceNumber = bag[index];
+        dispatch(setIsPlaceholderShowing({ colour, isPlaceholderShowing: false }));
+        dispatch(setDiceNumber({ colour, randomIndex: index }));
+        saveState();
+        if (dice) onDiceRoll(diceNumber);
+      }, DICE_PLACEHOLDER_DELAY);
+    },
+    [dispatch, saveState, store]
+  );
+};
