02e10cdf6d5fa771a606e3b45294e5d92ff13544
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2026-07-18 00:50:05 +0530

    refactor: migrate usePageLeaveBlocker to react-router v8

--- src/hooks/usePageLeaveBlocker.ts
+++ src/hooks/usePageLeaveBlocker.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useBlocker } from 'react-router-dom';
+import { useBlocker } from 'react-router';
 import { EXIT_MESSAGE } from '../pages/Play/components/Game/Game';
 
 export const usePageLeaveBlocker = (shouldBlock: boolean) => {
