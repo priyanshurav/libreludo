2fa39e27aafa70e65ed8ac59902a3919735e2ec6
Priyanshu Rav <77456300+priyanshurav@users.noreply.github.com>
2026-07-27 16:04:09 +0530

    refactor: mark player constants as const assertions

--- src/game/players/constants.ts
+++ src/game/players/constants.ts
@@ -5,11 +5,11 @@ export const playerColours = {
   red: '#ff0002ff',
   green: '#049645ff',
   yellow: '#ffde15ff',
-};
+} as const;
 
 export const MAX_PLAYER_NAME_LENGTH = 15;
 export const playerSequences: Record<TPlayerCount, TPlayerColour[]> = {
   two: ['blue', 'green'],
   three: ['blue', 'red', 'green'],
   four: ['blue', 'red', 'green', 'yellow'],
-};
+} as const;
